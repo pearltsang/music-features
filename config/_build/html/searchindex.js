d354c0fadd1c913865a64c1842f6d75d5ee7669b
Pearl Tsang <pearltsang@calvisitor-10-105-169-93.calvisitor.1918.berkeley.edu>
2016-10-25 15:21:07 -0700

    created config folder

---
+++ config/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:47,filenames:["api/api","index"],objects:{},objnames:{},objtypes:{},terms:{},titles:["API Reference","Music-features documentation"],titleterms:{api:0,content:1,document:1,featur:1,music:1,refer:0}})
