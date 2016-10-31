60e30e09644c5ffd52a5482e4db7d351c24a887b
Pearl Tsang <pearltsang@calvisitor-10-105-169-93.calvisitor.1918.berkeley.edu>
2016-10-31 14:55:09 -0700

    attempted to build html

--- config/_build/html/searchindex.js
+++ config/_build/html/searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({envversion:47,filenames:["api/api","index"],objects:{},objnames:{},objtypes:{},terms:{},titles:["API Reference","Music-features documentation"],titleterms:{api:0,content:1,document:1,featur:1,music:1,refer:0}})
+Search.setIndex({envversion:47,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{},titles:["Music-features documentation"],titleterms:{api:[],content:0,document:0,featur:0,music:0,refer:[]}})
